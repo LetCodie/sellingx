dee9e199b4539c9a34fd331a68d7b2dc5c91f300
radin-reth <radin.reth@gmail.com>
2017-08-05 00:02:15 +0700

    add route listing#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  resources :listings, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
