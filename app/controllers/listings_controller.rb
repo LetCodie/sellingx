5f7d543192d6b6da7ca1389589d0fe6e1e8f89b4
radin-reth <radin.reth@gmail.com>
2017-08-05 00:21:17 +0700

    add basic style

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,4 @@
+class ListingsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :listings, only: [:show]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :listings, only: [:show]
 end
