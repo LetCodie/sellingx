04a37c61f5553250896cb16e76a12bf955bb4640
radin-reth <radin.reth@gmail.com>
2017-08-11 23:49:15 +0700

    apply slick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,13 @@
 //= require jquery/dist/jquery.min
 //= require bootstrap/dist/js/bootstrap.min
+//= require slick.min
+
+$(document).ready(function(){
+  $('.slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    autoplay: true,
+    dots: true,
+  });
+});
