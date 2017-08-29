aba720cc45a7c82736acabec3d4358943e66058c
radin-reth <radin.reth@gmail.com>
2017-08-30 00:01:34 +0700

    add join free button

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,4 @@
+class AccountsController < ApplicationController
+  def register
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get 'accounts/login', to: 'accounts#login'
+  get 'accounts/register', to: 'accounts#register'
+
   resources :listings, only: [:show]
   resources :products, only: [:show] do
     get 'search', on: :collection
